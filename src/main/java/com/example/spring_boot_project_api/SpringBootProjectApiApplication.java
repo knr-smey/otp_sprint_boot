13a05252a47c3f6e684812a1a693cc2c27c0383d
knr-smey <krisjenxz@gmail.com>
2026-08-22 15:53:15 +0700

    add regiter text

--- src/main/java/com/example/spring_boot_project_api/SpringBootProjectApiApplication.java
+++ src/main/java/com/example/spring_boot_project_api/SpringBootProjectApiApplication.java
@@ -8,6 +8,7 @@ public class SpringBootProjectApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApiApplication.class, args);
+		System.out.println("Hi how are you?");
 		System.out.println("Spring Boot Project API is running...");
 		System.out.print("Login");
 	}
